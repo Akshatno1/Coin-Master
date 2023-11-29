00de5d55afb7193f05bd6b3f18e58baf1e2b4f5b
Akshat Sharma <aks30092001@gmail.com>
2023-11-30 00:02:17 +0530

    feat: created coin master game

---
+++ android/src/com/akshat/coinmaster/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.akshat.coinmaster.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.akshat.coinmaster.game.CoinMaster;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMaster(), config);
+	}
+}
